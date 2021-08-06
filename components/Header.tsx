aef7100d41aab1cb18b399fc1619fb2c7732632f
Aayush Aayron Deo <aayushaayron@gmail.com>
2021-08-06 23:07:12 +0400

    add mobile navbar button and animation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import { createUseStyles } from "react-jss"
 
 const useStyles = createUseStyles({
@@ -15,18 +16,61 @@ const useStyles = createUseStyles({
     },
     name: {
         fontWeight: "bold",
-        fontSize: "18pt"
+        fontSize: "16pt",
+        textDecoration: "none",
+        color: "black"
     },
-    '@media screen and (max-width: 360px)': {
+    navItem: {
+        paddingLeft: 15 
+    },
+    navContainer: {
+            display: "inline"
+    },
+    navMobileButton: {
+        display: "none"
+    },
+    down: {
+        width: "20px",
+        animation: "$turnDown 250ms linear forwards",
+        transform: "translateY(-10%)"
+    },
+    "@keyframes turnDown": {
+        "0%": {
+            transform: "rotateZ(180deg)"
+        },
+        "100%": {
+            transform: "rotateZ(360deg)"
+        }
+    },
+    up: {
+        width: "20px",
+        animation: "$turnUp 250ms linear forwards",
+        transform: "translateY(-10%)"
+    },
+    "@keyframes turnUp": {
+        "0%": {
+            transform: "rotateZ(0deg)"
+        },
+        "100%": {
+            transform: "rotateZ(180deg)"
+        }
+    },
+    '@media screen and (max-width: 549px)': { // mobile
         content: {
             width: "90%",
             padding: "15px"
         },
         name: {
             fontSize: "12pt"
+        },
+        navContainer: {
+            display: "none"
+        },
+        navMobileButton: {
+            display: "inline"
         }
     },
-    '@media screen and (min-width: 361px) and (max-width: 768px)': {
+    '@media screen and (min-width: 550px) and (max-width: 768px)': { // tablet
         content: {
             width: "90%",
             padding: "15px"
@@ -37,12 +81,52 @@ const useStyles = createUseStyles({
     }
 })
 
+const navItems = [
+    {
+        text: "Projects"
+    },
+    {
+        text: "Work Experience"
+    },
+    {
+        text: "Skills"
+    },
+    {
+        text: "Contact"
+    }
+]
+
 export default function Header() {
     const classes = useStyles()
+
+    const [arrowDirection, setArrowDirection] = useState(1)
+    const [arrowOrientation, setArrowOrientation] = useState(classes.down)
+
+
+    useEffect(() => {
+        if (arrowDirection == 1) {
+            setArrowOrientation(classes.down)
+        } else {
+            setArrowOrientation(classes.up)
+        }
+    }, [arrowDirection])
+
     return <div className={classes.container}>
         <div className={classes.content}>
-        <div className={classes.name}>Aayush Aayron Deo</div>
-        <div>Home</div>
+        <a href="/" className={classes.name}>Aayush Aayron Deo</a>
+
+        <span onClick={() => setArrowDirection(arrowDirection * -1)} className={classes.navMobileButton}>
+            <img className={arrowOrientation} src="arrow.png" alt="" />
+        </span>
+
+        <span className={classes.navContainer}>
+            {
+                navItems.map((navItem, index) => {
+                    return <span className={classes.navItem} key={index}>{navItem.text}</span>
+                })
+            }
+        </span>
+        
         </div>
     </div>
 }
