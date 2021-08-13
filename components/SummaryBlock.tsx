6e52aa2f1ff81f3a3c09e4a13d561ffc8a7b9dcb
Aayush Aayron Deo <aayushaayron@gmail.com>
2021-08-13 23:36:15 +0400

    refactor summary block code

--- components/SummaryBlock.tsx
+++ components/SummaryBlock.tsx
@@ -10,39 +10,15 @@ const useStyles = createUseStyles({
     justifyContent: "center",
     alignItems: "center",
   },
-  text: {
-    width: "70%",
-    transform: "translateY(-40%)",
-  },
-  p: {
-    margin: 0,
-  },
-  name: {
-    margin: 0,
-    fontWeight: "bold",
-    color: "#E2E8CE",
-    fontSize: "24pt",
-    textShadow: "2px 2px 2px grey",
-  },
-  summary: {
-    margin: 0,
-    color: "#E2E8CE",
-    fontSize: "18pt",
-    textShadow: "2px 2px 2px grey",
-  },
+  
 });
 
-export default function SummaryBlock() {
+export default function SummaryBlock({content}: {content: any}) {
   const classes = useStyles();
 
   return (
     <div className={classes.container}>
-      <div className={classes.text}>
-        <p className={classes.name}>I am Aayush!</p>
-        <p className={classes.summary}>
-          I am a full stack web and mobile app developer
-        </p>
-      </div>
+      {content}
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,39 @@ import Header from "../components/Header";
 import SummaryBlock from "../components/SummaryBlock";
 
 const useStyles = createUseStyles({
-  container: {},
+  text: {
+    width: "70%",
+    transform: "translateY(-40%)",
+  },
+  name: {
+    margin: 0,
+    fontWeight: "bold",
+    color: "#E2E8CE",
+    fontSize: "24pt",
+    textShadow: "2px 2px 2px grey",
+  },
+  summary: {
+    margin: 0,
+    color: "#E2E8CE",
+    fontSize: "18pt",
+    textShadow: "2px 2px 2px grey",
+  },
 });
 
 export default function Home() {
   const classes = useStyles();
 
+  const summaryBlockContent = <div className={classes.text}>
+        <p className={classes.name}>I am Aayush!</p>
+        <p className={classes.summary}>
+          I am a full stack web and mobile app developer
+        </p>
+      </div>
+
   return (
-    <div className={classes.container}>
+    <div>
       <Title />
-      <SummaryBlock />
+      <SummaryBlock content={summaryBlockContent} />
     </div>
   );
 }
