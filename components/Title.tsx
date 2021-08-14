a9dd593584b6ed583cf577f93487778ba8684380
Aayush Aayron Deo <aayushaayron@gmail.com>
2021-08-14 11:43:21 +0400

    add link preview image

--- components/Title.tsx
+++ components/Title.tsx
@@ -8,6 +8,7 @@ export default function Title({ title = "" }: { title?: string }) {
       <title>{titleText}</title>
       <meta name="description" content="Aayush Aayron Deo is a full stack web and mobile application developer. Currently Aayush is a senior at NYU Abu Dhabi. Aayush’s development stack includes, ReactJS, NextJS, MongoDB, Firebase, Google Cloud, React Native and ExpressJS among others" />
       <meta name="keywords" content="web developer, aayush aayron deo, reactjs, react native, expressjs, freelance web developer, mongodb, firebase, google cloud, aws, nextjs, MERN stack, mobile app developer, devops, youtube tutorials" />
+      <meta property="og:image" content="https://codingthesmartway.com/wp-content/uploads/2019/01/mern_logo.png" />
     </Head>
   );
 }
