b3de6744d3ac0b25c6a4f4af5718837aa589d70a
Aayush Aayron Deo <aayushaayron@gmail.com>
2021-08-14 11:15:20 +0400

    add meta tags

--- components/Title.tsx
+++ components/Title.tsx
@@ -6,6 +6,8 @@ export default function Title({ title = "" }: { title?: string }) {
   return (
     <Head>
       <title>{titleText}</title>
+      <meta name="description" content="Aayush Aayron Deo is a full stack web and mobile application developer. Currently Aayush is a senior at NYU Abu Dhabi. Aayush’s development stack includes, ReactJS, NextJS, MongoDB, Firebase, Google Cloud, React Native and ExpressJS among others" />
+      <meta name="keywords" content="web developer, aayush aayron deo, reactjs, react native, expressjs, freelance web developer, mongodb, firebase, google cloud, aws, nextjs, MERN stack, mobile app developer, devops, youtube tutorials" />
     </Head>
   );
 }
